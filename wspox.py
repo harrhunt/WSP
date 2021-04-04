c0db898b64e9ef2147f2467af8b1d26a9c2841a5
harrhunt <harrhunt@isu.edu>
2021-04-04 08:07:35 -0600

    new testing code

--- wspox.py
+++ wspox.py
@@ -1,10 +1,23 @@
 from wsp import *
 from oxford import *
+from nltk.corpus import wordnet as wn
 
 if __name__ == '__main__':
-    dog = WSG('plane')
-    definitions = get_all_coarse_defs('plane')
-    data = WSPComparer.group_by_closest_definition(dog, definitions)
+    trumpet = WSG('trumpet')
+    trumpeting = WSG('trumpeting')
+    trumpet_synset = wn.synsets('trumpet')
+    trumpeting_synset = wn.synsets('trumpeting')
+    print(trumpet_synset)
+    print(trumpeting_synset)
+    definitions = get_all_coarse_defs('trumpet')
+    data = WSPComparer.group_by_closest_definition(trumpet, definitions)
+    for definition in data:
+        print(f"OXFORD_DEFINITION: '{definition}'")
+        for wsp in data[definition]:
+            print(f"{wsp.name}->'{wsp.definition}'")
+        print("\n")
+    definitions = get_all_coarse_defs('trumpeting')
+    data = WSPComparer.group_by_closest_definition(trumpeting, definitions)
     for definition in data:
         print(f"OXFORD_DEFINITION: '{definition}'")
         for wsp in data[definition]:
